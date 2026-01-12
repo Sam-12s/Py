e0aad589224019073764d0add9e68c1b3ef2ebef
Sam-12s <tidianeyonkeu515@gmail.com>
2026-01-13 00:34:36 +0100

    Fix proxy assignment in AsyncClient context

--- scripts/XB1.py
+++ scripts/XB1.py
@@ -319,7 +319,7 @@ async def process_prefix(prefix):
 
             # 🔑 NEW CLIENT = NEW TLS HANDSHAKE
             async with httpx.AsyncClient(
-                    proxies=proxy,
+                    proxy=proxy,
                     timeout=httpx.Timeout(50.0, connect=50.0),
                     limits=httpx.Limits(
                         max_connections=68,
