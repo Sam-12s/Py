ec35d85696543c4a9a007f91c27979d31113bc29
Sam-12s <tidianeyonkeu515@gmail.com>
2026-03-01 00:33:52 +0100

    Update prefix_generator.js

--- prefix_generator.js
+++ prefix_generator.js
@@ -13,7 +13,7 @@ const agent = new Agent({
     keepAliveMaxTimeout: 60_000
 });
 // 🔥 CONTROL HOW MANY PREFIXES YOU WANT
-const TOTAL_PREFIXES_TARGET = 25;
+const TOTAL_PREFIXES_TARGET = 15;
 
 // Prefix storage
 let PREFIXES = [];
