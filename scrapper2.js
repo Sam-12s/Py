784dcabbd660a8bd1ef0387681412d115f796102
Sam-12s <tidianeyonkeu515@gmail.com>
2026-03-01 21:28:55 +0100

    Update scrapper2.js

--- scrapper2.js
+++ scrapper2.js
@@ -55,7 +55,7 @@ async function uploadDbToDrive() {
 
   const fileMetadata = {
     name: `output_${Date.now()}.db`,
-    parents: ["YOUR_FOLDER_ID"]
+    parents: ["1GJ13uUpHRvY0uEAZbXbhL4S1YNTjU7NR"]
   };
 
   const media = {
@@ -763,7 +763,7 @@ const { chromium } = require("playwright");
 
   try {
     const PREFIXES = [
-    "ST5",
+    "G1U",
 ];
 
     const state = {};
